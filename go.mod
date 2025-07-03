1d58252a4ef80be82a05796ef1b72286bfa63cc2
Leandro Lombi <leandro.lombi@synchro.com.br>
2025-07-03 08:44:37 -0300

    Create get API and implementation to return hello world

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/leandro-lombi/http_test.git
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/leandro-lombi/http_test.git/api"
+)
+
+func main() {
+	http.HandleFunc("/hello-world", api.HelloWorld)
+	http.ListenAndServe(":8080", nil)
+}
